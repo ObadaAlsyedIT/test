a9ac3ddeee1669d6db7ea6ba929b242d2f8513be
ObadaAlsyedIT <82112879+ObadaAlsyedIT@users.noreply.github.com>
2021-09-22 08:53:05 +0300

    Add files via upload

--- task4.js
+++ task4.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",function(){
+// document.addEventListener("DOMContentLoaded",function(){
     document.querySelector("form").onsubmit = function(){
         let emojis = [0x1F600, 0x1F604, 0x1F34A, 0x1F344, 0x1F37F, 0x1F363, 0x1F370, 0x1F355,
             0x1F354, 0x1F35F, 0x1F6C0, 0x1F48E, 0x1F5FA, 0x23F0, 0x1F579, 0x1F628,
@@ -33,5 +33,5 @@ document.addEventListener("DOMContentLoaded",function(){
          }
         return false;
     }
-})
+// })
 
